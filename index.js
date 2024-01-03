059347dffc1eb2c83791e007d2ae73e2fa083719
Siddhartha kumar singh <siddharthakumarsingh946@gmail.com>
2024-01-03 14:29:42 +0530

    api setup

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 const express = require("express")
 const mongoose = require("mongoose");
 const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/config");
-
+const postRouter = require("./routes/postRoute")
 
 const app = express()
+app.use(express.json());
 
 const mongoURL= `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
 
 const connectWithRetry = () =>{
-    mongoose.connect(mongoURL,{
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }).then(()=> console.log("successfully connected to database")).catch((e)=> console.log(e),
-    setTimeout(connectWithRetry,5000)
-    )    
-}
+    mongoose.connect(mongoURL)
+      .then(() => console.log("Successfully connected to the database"))
+      .catch((e) => {
+        console.error(e);
+        setTimeout(connectWithRetry, 5000);
+      });
+};
+connectWithRetry()
 
 app.get("/",(req,res)=>{
     res.send("<h2>Hello there!!</h2>")
 })
+app.use("/api/v1/posts",postRouter)
 const port = process.env.port || 3000;
 
 app.listen(port,()=> console.log(`listening on port ${port}`))
